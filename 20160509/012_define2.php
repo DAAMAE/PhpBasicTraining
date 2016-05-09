87854c01c4c270ca92aa3a76f4a1f9ef365bcbd2
Maeda <shige.shige.0903114@gmail.com>
2016-05-09 18:10:03 +0900

    20160509

---
+++ 20160509/012_define2.php
@@ -0,0 +1,6 @@
+<?php
+define("kazu", 8101919);
+echo kazu ."\n";
+define("kazu", 114514);
+echo kazu ."\n";
+?>
---
+++ 20160509/017_name.php
@@ -0,0 +1,8 @@
+<?php
+// 変数に文字列を格納
+$str_name1 = "maeda";
+$str_name2 = "shigeki";
+
+// 文字列を連結して出力
+echo $str_name1 . $str_name2 "だった。\n";
+?>
